c1d5775e0ba55e66e9500d44756a380521cd5d3c
Denis Markov <da.marckov2015@yandex.ru>
2025-07-27 02:29:15 +0700

    change style

--- rw_visual.py
+++ rw_visual.py
@@ -4,7 +4,7 @@ from random_walk import RandomWalk
 rw = RandomWalk()
 rw.fill_walk()
 
-plt.style.use("classic")
+plt.style.use("bmh")
 fig, ax = plt.subplots()
 ax.scatter(rw.x_values, rw.y_values, s=15)
 plt.show()
