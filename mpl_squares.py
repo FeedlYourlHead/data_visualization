4686e0a919e8de9348b73b08f6d456a1cd2f5b0e
Denis Markov <da.marckov2015@yandex.ru>
2025-07-26 23:51:54 +0700

    add comment

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 plt.style.use('bmh')
+# Назначение стиля \/
 fig, ax = plt.subplots()
+
 ax.plot(input_values, squares, linewidth=3)
 # Назначение загаловка диаграммы и меток осей.
 ax.set_title("Square Numbers", fontsize=24)
