bc2e9a4343db7ec95d8c0fbe91d8326899183390
Denis Markov <da.marckov2015@yandex.ru>
2025-07-27 04:10:21 +0700

    add some to random walk

--- rw_visual.py
+++ rw_visual.py
@@ -1,11 +1,23 @@
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from random_walk import RandomWalk
+while True:
+    rw = RandomWalk(50000)
+    rw.fill_walk()
 
-rw = RandomWalk()
-rw.fill_walk()
+    plt.style.use("bmh")
+    fig, ax = plt.subplots(figsize=(15, 9), dpi=128)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+               edgecolors="none", s=15)
+    # Выделение первой и последней точек.
+    ax.scatter(0, 0, c="green", edgecolors="none", s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1],
+               c='red', edgecolors='none', s=1)
 
-plt.style.use("bmh")
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
-
+    # Удаление осей
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.show()
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
