5244ddc7dbe4c5876b9ce157e13928aa594df6c9
Denis Markov <da.marckov2015@yandex.ru>
2025-07-28 01:46:49 +0700

    research method fill_between()

--- sitka_highs_lows.py
+++ sitka_highs_lows.py
@@ -16,15 +16,16 @@ with open(filename) as f:
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
-        low = int(row[6]) 
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
         lows.append(low)
 
 plt.style.use("bmh")
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c="red")
-plt.plot(dates, lows, c="blue")
+ax.plot(dates, highs, c="red", alpha=0.5)
+plt.plot(dates, lows, c="blue", alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
 plt.title("Daily high and low temperatures - 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
